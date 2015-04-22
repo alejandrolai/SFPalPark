11305e13841538e698bd6f812200ec5806fb6fd2
dmchung <dmchung@mail.sfsu.edu>
2015-04-22 02:10:55 -0700

    added Splash.java file

---
+++ app/src/main/java/com/alejandrolai/sfpark/Splash.java
@@ -0,0 +1,34 @@
+package com.alejandrolai.sfpark;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+
+/**
+ * Created by den on 4/21/2015.
+ */
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashSF);
+
+        final ImageView iv = (ImageView) findViewById(R.id.imageView);
+        Thread startTimer = new Thread(){
+            public void run(){
+                try {
+                    sleep(4000);
+                    Intent i = new Intent(Splash.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                } catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        startTimer.start();
+    }
+}
