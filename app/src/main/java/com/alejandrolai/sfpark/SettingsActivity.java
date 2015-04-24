22dea21e5def921fa513690f56d1e0305d939f07
ihsan_taha <ihsan_taha@yahoo.com>
2015-04-24 12:39:40 -0700

    About Page Test Sample (Ihsan)

---
+++ app/src/main/java/com/alejandrolai/sfpark/SettingsActivity.java
@@ -0,0 +1,59 @@
+package com.alejandrolai.sfpark;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TabHost;
+
+
+public class SettingsActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec("back");
+        tabSpec.setContent(R.id.back);
+        tabSpec.setIndicator("Back");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("reminder");
+        tabSpec.setContent(R.id.reminder);
+        tabSpec.setIndicator("Reminder");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("About");
+        tabSpec.setContent(R.id.about);
+        tabSpec.setIndicator("About");
+        tabHost.addTab(tabSpec);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_about, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
