27079504e919f6e7bd3f6c37fabe616b6f479017
danielmartin <dpmartin@mail.sfsu.edu>
2015-04-30 17:07:51 -0700

    Added a semi-working timer in the "Park Me" page.

--- app/src/main/java/com/alejandrolai/sfpark/Timer/ReminderActivity.java
+++ app/src/main/java/com/alejandrolai/sfpark/Timer/ReminderActivity.java
@@ -1,25 +1,81 @@
 package com.alejandrolai.sfpark.Timer;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.alejandrolai.sfpark.MainActivity;
 import com.alejandrolai.sfpark.R;
 
+import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
+
 
 public class ReminderActivity extends ActionBarActivity{
+
+    Button startButton, stopButton;
+    TextView textViewTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminder);
+
+        startButton = (Button) findViewById(R.id.buttonStart);
+        stopButton = (Button) findViewById(R.id.buttonStop);
+
+        textViewTime = (TextView) findViewById(R.id.viewTime);
+
+        textViewTime.setText("00:00:30");
+
+        final CounterClass timer = new CounterClass(30000, 1000);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer.start();
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer.cancel();
+            }
+        });
     }
 
+    //@TargetApi(Build.VERSION_CODES.GINGERBREAD)
+    public class CounterClass extends CountDownTimer{
+
+        public CounterClass(long millisInFuture, long countDownInterval){
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            long millisecs = millisUntilFinished;
+            String tick = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(millisecs),
+                    TimeUnit.MILLISECONDS.toMinutes(millisecs) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millisecs)),
+                    TimeUnit.MILLISECONDS.toSeconds(millisecs) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisecs)));
+
+            System.out.println(tick);
+            textViewTime.setText(tick);
+        }
+
+        @Override
+        public void onFinish() {
+            textViewTime.setText("Time is up!");
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -42,8 +98,19 @@ public class ReminderActivity extends ActionBarActivity{
 
         return super.onOptionsItemSelected(item);
     }
-    public void backtoMaps(View view){
 
+    public void createTimer(){
+        CountDownTimer countDownTimer;
+        boolean timerHasStarted = false;
+
+        //int
+
+        //long startTime =
+        long interval = 1 * 1000;
+
+    }
+
+    public void backtoMaps(View view){
 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
