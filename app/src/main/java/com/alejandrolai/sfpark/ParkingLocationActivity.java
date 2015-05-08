44948cc9ef055c1a18e7800347c41683c012bab3
Ami Parikh <amiparikh.masters@gmail.com>
2015-05-08 15:35:19 -0700

    Saving the theme chosen by the user in the Database. When a user opens the App the second time after choosing a Theme, the theme that was chosen earlier is restored.

--- app/src/main/java/com/alejandrolai/sfpark/ParkingLocationActivity.java
+++ app/src/main/java/com/alejandrolai/sfpark/ParkingLocationActivity.java
@@ -43,6 +43,7 @@ public class ParkingLocationActivity extends ActionBarActivity {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
         Date date = new Date();
         return dateFormat.format(date);
+
     }
 
 
